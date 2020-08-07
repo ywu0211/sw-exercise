bdf3132faccd7e71afcdf9091162cc5f8e33023e
Nathan Wu <nwu-consultant@scholastic.com>
2020-08-07 19:49:47 -0400

    fix redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: 'home', component: LayoutComponent
   },
   {
-    path: '', redirectTo: '/home', pathMatch: 'full'
+    path: '**', redirectTo: '/home', pathMatch: 'full'
   }
 ];
 
